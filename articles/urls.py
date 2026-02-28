8022c09159712ffd54e794e6f8658d3a48224f59
sineadezita <s.ezita@icloud.com>
2026-02-28 21:19:52 +0000

    Added article list and detail views, urls and templates

---
+++ articles/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('articles/', views.article_list, name='article_list'),
+    path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
+]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article, Category
 
 # Create your views here.
+
+def article_list(request):
+    """A view to show all published articles"""
+    articles = Article.objects.filter(is_published=True)
+    categories = Category.objects.all()
+
+    # Category filter
+    category_slug = request.GET.get('category')
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        articles = articles.filter(category=category)
+    
+    # Search
+    query = request.GET.get('q')
+    if query:
+        articles = articles.filter(title__icontains=query)
+
+    context = {
+        'articles': articles,
+        'categories': categories,
+    }
+    return render(request, 'articles/article_list.html', context)
+
+def article_detail(request, slug):
+    """ A view to show an individual article """
+    article = get_object_or_404(Article, slug=slug, is_published=True)
+
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/article_detail.html', context)
