0ed1ccbb1635f6c8e65c445599eb8a0c9a9a037b
sineadezita <s.ezita@icloud.com>
2026-03-01 18:35:56 +0000

    Added save article button to article list and save article URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('reading-list/', views.reading_list, name='reading_list'),
+    path('save-article/<int:article_id>/', views.save_article, name='save_article'),
 ]
