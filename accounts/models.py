f85dffeee81b1c08269ecc7e32d8c9edbdd19be0
sineadezita <s.ezita@icloud.com>
2026-03-01 14:38:13 +0000

    Added UserProfile model, signal and admin registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile'
+    )
+    bio = models.TextField(blank=True)
+    saved_articles = models.ManyToManyField(
+        'articles.Article', blank=True, related_name='saved_by'
+    )
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
