31aebb5b63e4d844f53d19bdaf9c9ca18cd5650d
sineadezita <s.ezita@icloud.com>
2026-03-01 17:20:55 +0000

    Added save article finction to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 
@@ -22,3 +24,19 @@ def reading_list(request):
         'saved_articles': saved_articles
     }
     return render(request, 'accounts/reading_list.html', context)
+
+@login_required
+def save_article(request, article_id):
+    """ Save or unsave an article to the user's reading list """
+    from articles.models import Article
+    article = get_object_or_404(Article, id=article_id)
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if article in profile.saved_articles.all():
+        profile.saved_articles.remove(article)
+        messages.success(request, f'Removed {article.title} from your reading list')
+    else:
+        profile.saved_articles.add(article)
+        messages.success(request, f'Added {article.title} to your reading list')
+
+    return redirect('articles:article_list')
