6757107440c1b6b12e08e527dae6bac7b99b47da
sineadezita <s.ezita@icloud.com>
2026-03-02 19:58:41 +0000

    Added Comment model, migrations and admin registration

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Comment
 
 # Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'article', 'created_at', 'is_approved']
+    list_filter = ['is_approved']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(is_approved=True)
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from articles.models import Article
 
 # Create your models here.
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='comments'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_at']
+    
+    def __str__(self):
+        return f'Comment by {self.author} on {self.article}'
