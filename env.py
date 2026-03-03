773bc26907a1b791f550db8c29bc384d9b21434c
sineadezita <s.ezita@icloud.com>
2026-03-03 14:21:07 +0000

    Removed env.py from git tracking, rotated secret keys

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ['SECRET_KEY'] = 'django-insecure-z6_$leij0yp#@jz-v1jgf$-q4o(b3s#a%t%_)3$gn&gr$5q2_f'
-os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51T6sbB1yPyR5aWRDoBwJkqpJhqwJvNEZvRRqFoPZifgfoVbfCSnIBkwgRYfw3UVyOyofWSDnFq8fUYP9nnV3uKBW00pZ97JIae'
-os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51T6sbB1yPyR5aWRDz4Y9BI0LKq5TJFGo2mLJcWg7oP7V2DILR7y1KVJsHpjvcStBEzLfwSTZgMFlI9iPfhPJXcTu001JQMJSFu'
-os.environ['STRIPE_WEBHOOK_SECRET'] = 'add stripe webhook secret'
-os.environ['STRIPE_PRICCE_ID'] = 'price_1T6tWU1yPyR5aWRD8kBoAnJH'
