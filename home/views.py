b372aa88a514716148d15d15fd684404ebed4891
sineadezita <s.ezita@icloud.com>
2026-03-06 22:55:08 +0000

    Built homepage with featured articles, events and premium subscribe banner

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from articles.models import Article
+from events.models import Event
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
-    
-    return render(request, 'home/index.html')
+    featured_articles = Article.objects.filter(
+        is_published='True').order_by('-created_at')[:4]
+    upcoming_events = Event.objects.all().order_by('event_date')[:3]
+
+    context = {
+        'featured_articles': featured_articles,
+        'upcoming_events': upcoming_events,
+    }
+    return render(request, 'home/index.html', context)
