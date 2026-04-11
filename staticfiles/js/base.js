d8a9930387b5d731e53e5a98e5d84cee4036761b
sineadezita <s.ezita@icloud.com>
2026-04-11 17:02:35 +0100

    Added hamburger menu and responsive navbar

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -16,3 +16,13 @@ setTimeout(function() {
         }
     });
  });
+
+ // Hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+if (hamburger) {
+   hamburger.addEventListener('click', function() {
+       navLinks.classList.toggle('open');
+   });
+}
