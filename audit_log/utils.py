491849c8cfbcbb045d683058f34b1155702282ab
sineadezita <s.ezita@icloud.com>
2026-04-12 23:10:10 +0100

    Final fixes before submission, validation, spacing and social links

--- audit_log/utils.py
+++ audit_log/utils.py
@@ -2,18 +2,18 @@ from .models import AuditLog
 
 
 def log_action(user, action, detail='', request=None):
-   """Log a user action to the audit log"""
-   ip_address = None
-   if request:
-       ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
-       if ip_address:
-           ip_address = ip_address.split(',')[0]
-       else:
-           ip_address = request.META.get('REMOTE_ADDR')
+    """Log a user action to the audit log"""
+    ip_address = None
+    if request:
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
+        if ip_address:
+            ip_address = ip_address.split(',')[0]
+        else:
+            ip_address = request.META.get('REMOTE_ADDR')
 
-   AuditLog.objects.create(
-       user=user,
-       action=action,
-       detail=detail,
-       ip_address=ip_address
-   )
+    AuditLog.objects.create(
+        user=user,
+        action=action,
+        detail=detail,
+        ip_address=ip_address
+    )
--- home/views.py
+++ home/views.py
@@ -5,31 +5,31 @@ from subscriptions.models import Subscription
 
 
 def index(request):
-   """A view to return the index page"""
-   featured_articles = Article.objects.filter(
+    """A view to return the index page"""
+    featured_articles = Article.objects.filter(
        is_published=True
-   ).order_by('-updated_at')[:5]
-   latest_articles = Article.objects.filter(
+    ).order_by('-created_at')[:3]
+    latest_articles = Article.objects.filter(
        is_published=True
-   ).order_by('-created_at')[:3]
-   upcoming_events = Event.objects.all().order_by('event_date')[:3]
+    ).order_by('-updated_at')[:5]
+    upcoming_events = Event.objects.all().order_by('event_date')[:3]
 
-   subscription = None
-   if request.user.is_authenticated:
-       try:
-           subscription = request.user.subscription
-       except Exception:
-           subscription = None
+    subscription = None
+    if request.user.is_authenticated:
+        try:
+            subscription = request.user.subscription
+        except Exception:
+            subscription = None
 
-   context = {
+    context = {
        'featured_articles': featured_articles,
        'latest_articles': latest_articles,
        'upcoming_events': upcoming_events,
        'subscription': subscription,
-   }
-   return render(request, 'home/index.html', context)
+    }
+    return render(request, 'home/index.html', context)
 
 
 def about(request):
-   """A view to return the about page"""
-   return render(request, 'home/about.html')
+    """A view to return the about page"""
+    return render(request, 'home/about.html')
